86e1ca709e2a582ca49388e30543dc62d5b57bf4
MyriadBugs <11000189+MyriadBugs@users.noreply.github.com>
2023-11-24 19:57:00 -0800

    Update PlayerControllerBPatch.cs
    Removed panel pivot changes (caused offset view, should be center 0.5,0.5).
    Scaled the hud so that it fills the screen
    Recentered the inventory grid.
    Slightly increased rendered aspect to combat squished view but not so much that it's cheat-ey.

--- Patches/PlayerControllerBPatch.cs
+++ Patches/PlayerControllerBPatch.cs
@@ -10,7 +10,7 @@ namespace LethalWidescreen.Patches
     {
         [HarmonyPostfix]
         [HarmonyPatch("OnEnable")]
-        private static void OnEnable()
+        private static void OnEnable(PlayerControllerB __instance)
         {
             GameObject ingamePlayerHud = GameObject.Find("Panel");
 
@@ -18,21 +18,30 @@ namespace LethalWidescreen.Patches
 
             panelRectTransform.anchorMin = new Vector2(0, 0);
             panelRectTransform.anchorMax = new Vector2(1, 1);
-
-            panelRectTransform.pivot = new Vector2(0, 1);
-
             panelRectTransform.anchoredPosition = Vector2.zero;
-
             panelRectTransform.sizeDelta = Vector2.zero;
-            
-            ManualLogSource mls = LethalWidescreen.mls;
-
 
             float xScaleFactor = Screen.width / (Screen.height * (16f / 9f));
+            panelRectTransform.localScale = new Vector3(xScaleFactor + 0.1f, 1f, 1f);
+
+            ManualLogSource mls = LethalWidescreen.mls;
             mls.LogInfo("!!! " + xScaleFactor + " set to xScaleFactor!");
 
+            //Scale the hud
+            UnityEngine.UI.AspectRatioFitter uiArf = __instance.playerHudUIContainer.GetComponent<AspectRatioFitter>();
+            uiArf.aspectRatio = (float)Screen.width / Screen.height;
 
-            panelRectTransform.localScale = new Vector3(xScaleFactor+0.1f, 1f, 1f);
+            CanvasScaler uiScaler = __instance.playerHudUIContainer.parent.GetComponent<CanvasScaler>();
+            uiScaler.scaleFactor = (float)Screen.width / Screen.height; //Approx aspect ratio seems to work for canvas scaling. Maybe coincidence.
+            uiScaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+
+            //Recenter Inventory Grid for 21:9
+            var inventory = __instance.playerHudUIContainer.Find("Inventory").GetComponent<RectTransform>();
+            inventory.anchorMax = new Vector2(0.0f, 1.0f);
+            inventory.anchorMin = new Vector2(1.04f, 0.1f);
+
+            //sliiightly bump camera aspect
+            __instance.gameplayCamera.aspect = 1.78f; //Slightly increase the aspect ratio. Actual 21:9 would be cheat-ey. Adds a few degrees of vision on either side to combat stretching further.
         }
 
         [HarmonyPostfix]
